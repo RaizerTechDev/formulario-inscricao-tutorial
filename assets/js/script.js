41af80e2719da3ddbc35d80cf0b03989053eba92
RafaRz76Dev <rafarz76dev@gmail.com>
2024-10-15 23:09:04 -0300

    :recycle:refactor: (folder./assets/js) file./acript.js and file./index.html and lipstick:feat:(folder./assets./css)file./styles.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const passwordInput = document.getElementById("passwordInput");
   const togglePassword = document.getElementById("togglePassword");
   const passwordStrengthText = document.getElementById("passwordStrengthText");
-  const passwordStrengthIndicator = document.getElementById("passwordStrengthIndicator");
+  const passwordStrengthIndicator = document.getElementById(
+    "passwordStrengthIndicator"
+  );
   const form = document.querySelector(".form--subscription");
   const submitButton = document.querySelector(".formButton");
 
   // Função para alternar a visibilidade da senha
   togglePassword.addEventListener("click", function () {
-    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    const type =
+      passwordInput.getAttribute("type") === "password" ? "text" : "password";
     passwordInput.setAttribute("type", type);
     this.classList.toggle("fa-eye-slash");
   });
@@ -32,63 +35,67 @@ document.addEventListener("DOMContentLoaded", function () {
   passwordInput.addEventListener("input", function () {
     const password = passwordInput.value;
     const strength = checkPasswordStrength(password);
-    let strengthText = '';
-    passwordStrengthIndicator.style.width = '0%';
+    let strengthText = "";
+    passwordStrengthIndicator.style.width = "0%";
 
     switch (strength) {
       case 0:
       case 1:
         strengthText = "Fraca";
-        passwordStrengthIndicator.style.backgroundColor = 'red';
-        passwordStrengthIndicator.style.width = '33%';
+        passwordStrengthIndicator.style.backgroundColor = "red";
+        passwordStrengthIndicator.style.width = "33%";
         break;
       case 2:
         strengthText = "Média";
-        passwordStrengthIndicator.style.backgroundColor = 'yellow';
-        passwordStrengthIndicator.style.width = '66%';
+        passwordStrengthIndicator.style.backgroundColor = "yellow";
+        passwordStrengthIndicator.style.width = "66%";
         break;
       case 3:
         strengthText = "Forte";
-        passwordStrengthIndicator.style.backgroundColor = 'green';
-        passwordStrengthIndicator.style.width = '100%';
+        passwordStrengthIndicator.style.backgroundColor = "green";
+        passwordStrengthIndicator.style.width = "100%";
         break;
     }
 
     passwordStrengthText.textContent = strengthText;
   });
 
-  // Função que remove a imagem de carregamento e retorna o texto original
-  function hideLoading() {
-    const loadingImage = document.querySelector(".footer__button--animation");
-    if (loadingImage) {
-      loadingImage.remove();
-    }
-    submitButton.style.display = 'inline-block';
-  }
+ // Função que adiciona a imagem de carregamento ao botão
+ const renderButton = {
+  image: '<img class="footer__button--animation" src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWI0azVpZzY1YzJqMDlxNjFlZHNhNmE0aGQ3dnhic2h4eGY2dmdhdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7bu3XilJ5BOiSGic/giphy.gif" alt="Loading">',
+  message: "SEND MESSAGE",
+};
 
   // Redirecionar para WhatsApp com os dados do formulário
   form.addEventListener("submit", function (e) {
     e.preventDefault(); // Impede o envio padrão do formulário
-
-    // Adiciona o GIF de carregamento
-    submitButton.style.display = 'none';
-    const loadingImage = document.createElement('img');
-    loadingImage.className = 'footer__button--animation';
-    loadingImage.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWI0azVpZzY1YzJqMDlxNjFlZHNhNmE0aGQ3dnhic2h4eGY2dmdhdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7bu3XilJ5BOiSGic/giphy.gif';
-    loadingImage.alt = "Loading";
-    submitButton.parentNode.insertBefore(loadingImage, submitButton.nextSibling);
+      // Adiciona o GIF de carregamento
+      submitButton.innerHTML = renderButton.image;
 
     const firstName = form.firstName.value;
     const lastName = form.lastName.value;
     const email = form.email.value;
+    const password = form.password.value;
     const interests = form.interests.value;
 
-    const whatsappUrl = `https://wa.me/5547999327137?text=Olá, estou retornando o Formulário-Tutorial: \n Meu Nome é: ${firstName} ${lastName} \n Meu E-mail: ${email } \n Meu interesse é na Área: ${interests}.`;
-
-    // Redireciona para o WhatsApp
-    window.open(whatsappUrl, '_blank');
+  const whatsappUrl = `https://wa.me/5547999327137?text=Olá, estou retornando o Formulário-Tutorial:%0A
+      Meu Nome: ${firstName} ${lastName}%0A 
+      E-mail: ${email}%0A 
+      Password: ${password}%0A
+      Interesse na Área: ${interests}.`;
 
     // Simula um tempo de espera antes de esconder a imagem de carregamento
-    setTimeout(hideLoading, 2000);
+    setTimeout(() => {
+      // Remove o spinner e volta ao texto original
+      submitButton.innerHTML = renderButton.message;
+      if (window.innerWidth > 768) {
+        // Para telas maiores que 768px, abrir em uma nova aba
+        window.open(whatsappUrl, "_blank");
+      } else {
+        // Para telas menores, abrir na mesma aba
+        window.open(whatsappUrl, "_self");
+      }
+    }, 2000);
+
   });
 });
